dc42d1f686354dcfef20875739d05f00b8490b8f
lucas <lucas.cardo123@hotmail.com>
2022-12-29 15:02:13 -0400

    Add coffee text span

--- lib/coffe_cup.dart
+++ lib/coffe_cup.dart
@@ -6,3 +6,4 @@ export 'features/assets/assets_unicorn.dart';
 export 'features/cards/coffee_card_theme.dart';
 export 'features/text/coffee_text.dart';
 export 'features/fields/coffee_search_field.dart';
+export 'features/text/coffee_text_span.dart';
---
+++ lib/features/text/coffee_text_span.dart
@@ -0,0 +1,20 @@
+import 'package:coffee_cup/coffe_cup.dart';
+import 'package:flutter/material.dart';
+import 'package:manga_easy_themes/manga_easy_themes.dart';
+
+class CoffeeTextSpan extends TextSpan {
+  final CoffeeTypography typography;
+  final Color? color;
+
+  const CoffeeTextSpan(
+      {this.typography = CoffeeTypography.body,
+      this.color,
+      super.text,
+      super.children});
+  @override
+  TextStyle? get style => const TextStyle().copyWith(
+        color: color ?? ThemeService.of.backgroundText,
+        fontSize: typography.size,
+        fontWeight: typography.isBold ? FontWeight.bold : FontWeight.w100,
+      );
+}
