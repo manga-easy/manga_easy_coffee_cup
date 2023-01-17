75c6be2ed74c73c204a9a5f699419d9f408f213e
lucas <lucas.cardo123@hotmail.com>
2023-01-17 15:24:56 -0400

    Create coffee asset image

---
+++ lib/features/images/coffee_asset_image.dart
@@ -0,0 +1,13 @@
+import 'package:coffee_cup/coffe_cup.dart';
+import 'package:flutter/material.dart';
+
+class CoffeeAssetImage extends AssetImage {
+  CoffeeAssetImage.horn(AssetsHorn asset)
+      : super(asset.path, package: CoffeeImage.package);
+
+  CoffeeAssetImage.unicorn(AssetsUnicorn asset)
+      : super(asset.path, package: CoffeeImage.package);
+
+  CoffeeAssetImage.state(AssetsState asset)
+      : super(asset.path, package: CoffeeImage.package);
+}
