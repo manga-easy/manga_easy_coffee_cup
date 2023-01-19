676b70cc539fdac88a24ee5b83ba2df72fe21797
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-01-19 15:36:20 -0300

    adding new unicorns

--- lib/features/assets/assets_unicorn.dart
+++ lib/features/assets/assets_unicorn.dart
@@ -6,7 +6,12 @@ enum AssetsUnicorn {
   lendo('$pathas/unicornio-lendo.png'),
   manutencao('$pathas/unicornio-manutencao.png'),
   sad('$pathas/unicornio-triste.png'),
-  fire('$pathas/unicornio_fogo.png');
+  fire('$pathas/unicornio_fogo.png'),
+  writing('$pathas/unicornio_escrevendo.png'),
+  scooter('$pathas/unicornio_lambreta.png'),
+  sucess('$pathas/unicornio_sucesso.png'),
+  error('$pathas/unicornio-erro.png'),
+  error404('$pathas/unicornio-erro.png');
 
   final String path;
   const AssetsUnicorn(this.path);
