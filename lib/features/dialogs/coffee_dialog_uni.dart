eccbeb069e76bf31546ab8af87b60749c5f029cb
lucas <lucas.cardo123@hotmail.com>
2023-02-08 15:08:39 -0400

    Add dialog unicorn

---
+++ lib/features/dialogs/coffee_dialog_uni.dart
@@ -0,0 +1,87 @@
+import 'package:coffee_cup/coffe_cup.dart';
+import 'package:flutter/material.dart';
+import 'package:manga_easy_themes/manga_easy_themes.dart';
+
+var _unicorn = AssetsUnicorn.alegre;
+var _middleText = '';
+
+class CoffeeDialogUnicorn extends StatelessWidget {
+  final String? middleText;
+  final String title;
+  final List<Widget> buttons;
+
+  const CoffeeDialogUnicorn({
+    super.key,
+    this.buttons = const [],
+    this.middleText,
+    required this.title,
+  });
+
+  void show(BuildContext context) {
+    _middleText = middleText ?? '';
+    showDialog(context: context, builder: build);
+  }
+
+  void error(BuildContext context) {
+    _unicorn = AssetsUnicorn.sad;
+    _middleText = middleText!.replaceAll('Exception:', '');
+    showDialog(context: context, builder: build);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: ThemeService.of.backgroundColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: ThemeService.of.borderRadius,
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CoffeeText(
+                text: title,
+                typography: CoffeeTypography.title,
+              ),
+              GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: Icon(
+                  Icons.close,
+                  color: ThemeService.of.backgroundText,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          CoffeeImage.unicorn(
+            _unicorn,
+            height: 150,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          _middleText.isNotEmpty
+              ? CoffeeText(text: _middleText)
+              : const SizedBox(),
+        ],
+      ),
+      actions: <Widget>[
+        ...buttons,
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: CoffeeButton(
+            label: 'OK',
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            onPress: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
