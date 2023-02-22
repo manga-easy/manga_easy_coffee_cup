2ea26274e18e74265d582a926b2934cccfeec878
lucas <lucas.cardo123@hotmail.com>
2023-02-22 09:48:04 -0400

    Add padding in coffeeButtontext

--- lib/features/buttons/coffee_button_text.dart
+++ lib/features/buttons/coffee_button_text.dart
@@ -4,17 +4,19 @@ import 'package:flutter/material.dart';
 class CoffeeButtonText extends StatelessWidget {
   final String text;
   final void Function()? onPressed;
+  final EdgeInsetsGeometry? padding;
 
   const CoffeeButtonText({
     super.key,
     this.onPressed,
     required this.text,
+    this.padding,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      style: TextButton.styleFrom(),
+      style: TextButton.styleFrom(padding: padding),
       onPressed: onPressed,
       child: CoffeeText(
         text: text,
