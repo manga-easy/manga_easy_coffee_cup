12601835638b01315a09abc77c13b9b1f62abbef
lucas <lucas.cardo123@hotmail.com>
2023-04-05 17:20:27 -0400

    export feature carousel

--- lib/coffe_cup.dart
+++ lib/coffe_cup.dart
@@ -17,3 +17,4 @@ export 'features/assets/assets_state.dart';
 export 'features/images/coffee_asset_image.dart';
 export 'features/buttons/coffee_button_text.dart';
 export 'features/dialogs/coffee_dialog_uni.dart';
+export 'features/carousel/export_carousel.dart';
