019cf3cc089702b7cf0c60023440057f19cb651d
lucas <lucas.cardo123@hotmail.com>
2023-05-15 13:48:34 -0400

    Add assets cat

---
+++ lib/features/assets/cats/assets_unicorn.dart
@@ -0,0 +1,12 @@
+const _path = 'lib/features/assets/cats';
+
+enum AssetsCats {
+  runningAfterHeart('$_path/gato_correndo_atras_coracao.gif'),
+  runningCircle('$_path/gato_rodando.gif'),
+  hugging('$_path/gato_se_abracado.gif'),
+  beingKnockedDown('$_path/gato_sendo_derrubado.gif'),
+  smart('$_path/gato_esperto.git');
+
+  final String path;
+  const AssetsCats(this.path);
+}
