cdeb19b0bd99fb35f8ddc3c6d4f74ae1abe43ff1
lucas <lucas.cardo123@hotmail.com>
2023-05-16 13:41:05 -0400

    Fix gato_esperto name file in assets cats

--- lib/features/assets/cats/assets_cats.dart
+++ lib/features/assets/cats/assets_cats.dart
@@ -5,7 +5,7 @@ enum AssetsCats {
   runningCircle('$_path/gato_rodando.gif'),
   hugging('$_path/gato_se_abracado.gif'),
   beingKnockedDown('$_path/gato_sendo_derrubado.gif'),
-  smart('$_path/gato_esperto.git');
+  smart('$_path/gato_esperto.gif');
 
   final String path;
   const AssetsCats(this.path);
