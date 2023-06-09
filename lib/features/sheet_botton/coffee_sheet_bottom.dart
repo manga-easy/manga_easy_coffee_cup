5bfc7c1c2ffdabc25f2288251576e04771da743c
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-06-08 23:48:37 -0300

    changed Coffee Sheet

--- lib/features/sheet_botton/coffee_sheet_bottom.dart
+++ lib/features/sheet_botton/coffee_sheet_bottom.dart
@@ -3,32 +3,30 @@ import 'package:manga_easy_themes/manga_easy_themes.dart';
 
 class CoffeeSheetBottom extends StatelessWidget {
   final List<Widget> children;
-  final Widget icon;
-  const CoffeeSheetBottom(
-      {super.key, required this.children, required this.icon});
+  const CoffeeSheetBottom({super.key, required this.children});
+
+  void show(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: ThemeService.of.backgroundColor,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+      ),
+      builder: (BuildContext context) {
+        return build(context);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => showModalBottomSheet<void>(
-        context: context,
-        isScrollControlled: true,
-        backgroundColor: ThemeService.of.backgroundColor,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-        ),
-        builder: (BuildContext context) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: children,
-          );
-        },
-      ),
-      borderRadius: BorderRadius.circular(99),
-      child: Padding(
-        padding: const EdgeInsets.all(4),
-        child: icon,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 20, 16, 16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: children,
       ),
     );
   }
