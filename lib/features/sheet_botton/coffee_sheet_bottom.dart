e9bf3f0911967041fc3d95ceec8fa4e94f2da21c
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-06-09 15:36:07 -0300

    Setting the Sheetbottom

--- lib/features/sheet_botton/coffee_sheet_bottom.dart
+++ lib/features/sheet_botton/coffee_sheet_bottom.dart
@@ -1,32 +1,55 @@
+import 'package:coffee_cup/coffe_cup.dart';
 import 'package:flutter/material.dart';
 import 'package:manga_easy_themes/manga_easy_themes.dart';
 
 class CoffeeSheetBottom extends StatelessWidget {
-  final List<Widget> children;
-  const CoffeeSheetBottom({super.key, required this.children});
+  final Widget title;
+  final Alignment? alignmentTitle;
+  final Widget body;
+  final Color? backgroundColor;
+  final CoffeeButton button;
+  const CoffeeSheetBottom({
+    super.key,
+    required this.title,
+    required this.body,
+    this.alignmentTitle,
+    this.backgroundColor,
+    required this.button,
+  });
 
   void show(BuildContext context) {
     showModalBottomSheet<void>(
       context: context,
       isScrollControlled: true,
-      backgroundColor: ThemeService.of.backgroundColor,
+      backgroundColor: backgroundColor ?? ThemeService.of.backgroundColor,
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
             topLeft: Radius.circular(20), topRight: Radius.circular(20)),
       ),
-      builder: (BuildContext context) {
-        return build(context);
-      },
+      builder: build,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 20, 16, 16),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: children,
+      padding: MediaQuery.of(context).viewInsets,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Align(
+              alignment: alignmentTitle ?? Alignment.topLeft,
+              child: title,
+            ),
+            const SizedBox(height: 24),
+            body,
+            const SizedBox(height: 16),
+            button,
+          ],
+        ),
       ),
     );
   }
