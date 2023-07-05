237334c1dbc1be61733bd46e6cb0765badd1ddf9
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-07-05 13:51:30 -0300

    changed Coffee Field

--- lib/features/fields/coffee_field.dart
+++ lib/features/fields/coffee_field.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_cup/coffe_cup.dart';
 import 'package:flutter/material.dart';
 import 'package:manga_easy_themes/manga_easy_themes.dart';
 
@@ -8,11 +9,8 @@ class CoffeeField extends StatelessWidget {
   final TextEditingController? controller;
   final Color? color;
   final bool lines;
-  final EdgeInsetsGeometry? contentPadding;
   final void Function(String)? onChanged;
   final Widget? suffixIcon;
-  final double? height;
-  final EdgeInsetsGeometry? margin;
   final void Function()? onTap;
   final void Function()? onEditingComplete;
   final EdgeInsets? scrollPadding;
@@ -28,11 +26,8 @@ class CoffeeField extends StatelessWidget {
       this.controller,
       this.color,
       this.lines = false,
-      this.contentPadding,
       this.onChanged,
       this.suffixIcon,
-      this.height,
-      this.margin,
       this.onTap,
       this.onEditingComplete,
       this.scrollPadding,
@@ -42,54 +37,49 @@ class CoffeeField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height ?? 54,
-      padding: margin,
-      child: TextFormField(
-        scrollPadding: scrollPadding ?? const EdgeInsets.all(20),
-        onTap: onTap,
-        onChanged: onChanged,
-        controller: controller,
-        obscureText: obscureText,
-        onEditingComplete: onEditingComplete,
-        minLines: lines ? 10 : null,
-        maxLines: lines ? 200 : 1,
-        cursorColor: color ?? ThemeService.of.backgroundText,
-        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: color ?? ThemeService.of.backgroundText,
-              fontWeight: fontWeight ?? FontWeight.normal,
+    return CoffeeContainer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CoffeeText(
+            text: 'Senha atual',
+            color: ThemeService.of.primaryColor,
+          ),
+          const SizedBox(height: 4),
+          TextFormField(
+            scrollPadding: scrollPadding ?? const EdgeInsets.all(20),
+            onTap: onTap,
+            onChanged: onChanged,
+            controller: controller,
+            obscureText: obscureText,
+            onEditingComplete: onEditingComplete,
+            minLines: lines ? 10 : null,
+            maxLines: lines ? 200 : 1,
+            cursorColor: color ?? ThemeService.of.backgroundText,
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  fontWeight: fontWeight ?? FontWeight.normal,
+                ),
+            initialValue: initText,
+            decoration: InputDecoration(
+              isDense: true,
+              contentPadding: EdgeInsets.zero,
+              border: InputBorder.none,
+              suffixIcon: suffixIcon,
+              prefixIcon: icone != null
+                  ? Icon(
+                      icone,
+                      color: color ?? ThemeService.of.backgroundIcon,
+                    )
+                  : null,
+              hintText: hintText,
+              hintStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: color ??
+                        ThemeService.of.backgroundText.withOpacity(0.6),
+                    fontWeight: fontWeightHint ?? FontWeight.normal,
+                  ),
             ),
-        initialValue: initText,
-        decoration: InputDecoration(
-          contentPadding: contentPadding,
-          filled: true,
-          prefixIcon: icone != null
-              ? Icon(
-                  icone,
-                  color: color ?? ThemeService.of.backgroundIcon,
-                )
-              : null,
-          hintText: hintText,
-          hintStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: color ?? ThemeService.of.backgroundText.withOpacity(0.6),
-              fontWeight: fontWeightHint ?? FontWeight.normal),
-          border: OutlineInputBorder(
-            borderSide: BorderSide(
-                color: color ?? ThemeService.of.primaryColor, width: 2),
-            borderRadius: BorderRadius.circular(20),
           ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-                color: color ?? ThemeService.of.primaryColor, width: 2),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-                color: color ?? ThemeService.of.backgroundText, width: 2),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          suffixIcon: suffixIcon,
-        ),
+        ],
       ),
     );
   }
