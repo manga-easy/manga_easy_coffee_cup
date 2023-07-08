e860e030f5e7baca3bf04c9049b1f7ff11401da9
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-07-08 20:21:47 -0300

    add FieldText
    entity to configure text elements on CoffeeField

---
+++ lib/features/fields/styles/field_text.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class FieldText {
+  final String? title;
+  final String? hintText;
+  final String? initText;
+  final Color? textColor;
+  final Color? hintColor;
+  final Color? titleColor;
+  final bool? obscure;
+  final FontWeight? textWeight;
+  final FontWeight? hintWeight;
+
+  FieldText({
+    this.title,
+    this.hintText,
+    this.initText,
+    this.textColor,
+    this.hintColor,
+    this.titleColor,
+    this.obscure,
+    this.textWeight,
+    this.hintWeight,
+  });
+}
